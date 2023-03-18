d7e7ddb6c3d78f934b9169285de0268c616bef68
Eden Vidal <edenvidal@gmail.com>
2023-03-18 23:33:02 +0200

    Update rnbw-banner.js

--- rnbw-banner.js
+++ rnbw-banner.js
@@ -1,11 +1,11 @@
 const bannerTemplate = `
-<nav class="box-l padding-l direction-column box border-bottom">
+<nav class="box-l padding-l direction-column box border-bottom justify-stretch">
     <a href="https://rnbw.company" class="box gap-l">
-    <svg-icon src="https://rnbw.company/images/rnbw.svg"></svg-icon>
-        <p class="text-s">high-quality,</br> functional design</br>and development tools</p>
+        <svg-icon src="https://rnbw.company/images/rnbw.svg"></svg-icon>
+        <div class="text-s">high-quality,</br> functional design</br>and development tools</div>
     </a>
     <div class="gap-l box justify-end">
-        <a href="https://rnbw.company/" class="align-center direction-row">
+        <a href="https://rnbw.company/" class="direction-row">
         try rainbow if you have the courage ↗
         </a>
     </div>
