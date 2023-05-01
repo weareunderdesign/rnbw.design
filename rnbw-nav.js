6ccf3704ab05ae95717facd86810291839ea281f
Eden Vidal <edenvidal@gmail.com>
2023-05-01 12:20:43 +0300

    Update rnbw-nav.js

--- rnbw-nav.js
+++ rnbw-nav.js
@@ -2,12 +2,12 @@ const navTemplate = `
                 <div class="direction-column box-l gap-m">
                     <div class="box">
                         <a
-                            href="https://rnbw.dev"
+                            href="https://rnbw.company/signup"
                             target="_blank"
-                            class="padding-l gap-s radius-s background-primary border">
+                            class="padding-l gap-m radius-s background-primary border">
                             <svg-icon
                                 src="https://raincons.rnbw.dev/icons/oval.svg"
-                                class="icon-s"></svg-icon>
+                                class="icon-m"></svg-icon>
                             <div>
                                 <span class="text-l">start ↗</span>
                                 <p class="text-s">get it going with rnbw.</p>
@@ -18,10 +18,10 @@ const navTemplate = `
                         <a
                             href="https://github.com/rnbwdev/rnbw"
                             target="_blank"
-                            class="padding-l gap-s radius-s background-primary border">
+                            class="padding-l gap-m radius-s background-primary border">
                             <svg-icon
                                 src="https://raincons.rnbw.dev/icons/github.svg"
-                                class="icon-s"></svg-icon>
+                                class="icon-m"></svg-icon>
                             <div>
                                 <span class="text-l">GitHub ↗</span>
                                 <p class="text-s">
@@ -34,10 +34,10 @@ const navTemplate = `
                         <a
                             href="https://guide.rnbw.dev/"
                             target="_blank"
-                            class="padding-l gap-s radius-s background-primary border">
+                            class="padding-l gap-m radius-s background-primary border">
                             <svg-icon
                                 src="https://raincons.rnbw.dev/icons/library.svg"
-                                class="icon-s"></svg-icon>
+                                class="icon-m"></svg-icon>
                             <div>
                                 <span class="text-l">guide ↗</span>
                                 <p class="text-s">learn how to use.</p>
@@ -46,12 +46,12 @@ const navTemplate = `
                     </div>
                     <div class="box">
                         <a
-                            href="https://discord.gg/cKHxeyR2/"
+                            href="https://discord.gg/HycXz8TJkd/"
                             target="_blank"
-                            class="padding-l gap-s radius-s background-primary border">
+                            class="padding-l gap-m radius-s background-primary border">
                             <svg-icon
                                 src="https://raincons.rnbw.dev/icons/people.svg"
-                                class="icon-s"></svg-icon>
+                                class="icon-m"></svg-icon>
                             <div>
                                 <span class="text-l">community ↗</span>
                                 <p class="text-s">be part of it.</p>
