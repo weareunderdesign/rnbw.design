b767e78a14d10320ff9df8a9c585c187dad32228
Eden Vidal <edenvidal@gmail.com>
2023-09-20 17:14:25 +0300

    Update rnbw-footer.js

--- rnbw-footer.js
+++ rnbw-footer.js
@@ -15,7 +15,7 @@ const footerTemplate = `
         </div>
         <div class="column gap-s box">
 
-            <a href="https://discord.gg/HycXz8TJkd" target="_blank">discord ↗</a>
+            <a href="https://discord.gg/5CEf42mF/" target="_blank">discord ↗</a>
             <a href="https://twitter.com/rnbwdev" target="_blank">twitter ↗</a>
             <a href="https://github.com/rnbwdev" target="_blank">github ↗</a>
             <a href="mailto:hello@rnbw.company">get in touch</a>
